8ea7138151fb8a5958ed65b8a8c3fa828a104657
Vihanga <vihangahw@gmail.com>
2025-04-11 00:43:07 +0530

    index js

---
+++ Backend/models/index.js
@@ -0,0 +1,21 @@
+const database = require('../config/db');
+const RegisteredUser = require('./User');
+const ApiUsageLog = require('./ApiUsageLog');
+
+const initializeDatabase = async () => {
+    try {
+        await database.authenticate();
+        console.log('Connected to SQLite database');
+
+        await database.sync({ alter: true }); 
+        console.log('All models synced successfully');
+    } catch (error) {
+        console.error('Database connection error:', error);
+    }
+};
+
+module.exports = {
+    initializeDatabase,
+    RegisteredUser,
+    ApiUsageLog,
+};
